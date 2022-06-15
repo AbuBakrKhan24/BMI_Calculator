9f8ef4fa8f4e9ecf8a128ba187f02ae1aaac77da
AbuBakrKhan24 <abubakrkhan224@gmail.com>
2022-06-15 15:48:58 +0200

    done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function evalBMI() {
   let weight = document.bmi_form.pounds.value,
-    height = document.bmi_form.inches.value;
+    height = document.bmi_form.inches.value / 100;
   document.bmi_form.bmi.value = parseInt(weight / (height * height));
 }
